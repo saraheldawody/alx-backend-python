af66ebda80509cb242a8f150b6a3384682859964
saraheldawody <saraheldawody@hotmail.com>
2025-06-04 23:42:29 +0300

    chat models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -15,7 +15,7 @@ class User(AbstractUser):
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=150, blank=True)
     last_name = models.CharField(max_length=150, blank=True)
-
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
     def __str__(self):
         return self.username
 
