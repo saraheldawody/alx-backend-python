fb20cac48ce71338299ef9913d652041e3ee3881
saraheldawody <saraheldawody@hotmail.com>
2025-06-15 22:10:47 +0300

    add log requests middleware

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 ]
 AUTH_USER_MODEL = 'chats.User'
 MIDDLEWARE = [
+    "chats.middleware.RequestLoggingMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -58,7 +59,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "chats.middleware.RequestLoggingMiddleware"
 ]
 
 # REST Framework global settings
